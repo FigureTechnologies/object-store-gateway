60120e461ac6a132414d57e183a5a0495a8baf4c
Pierce Trey <ptrey@figure.com>
2022-03-31 11:35:26 -0600

    try publishing proto lib to maven as well

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,14 @@ version = project.property("version")?.takeIf { it != "unspecified" } ?: "1.0-SN
 plugins {
 	Plugins.Idea.addTo(this)
 	Plugins.NexusPublishing.addTo(this)
+	Plugins.MavenPublish.addTo(this)
+	Plugins.Signing.addTo(this)
+
+	kotlin("jvm") version "1.6.10"
+}
+
+repositories {
+	mavenCentral()
 }
 
 nexusPublishing {
@@ -14,6 +22,58 @@ nexusPublishing {
 			username.set(findProject("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME"))
 			password.set(findProject("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD"))
             stagingProfileId.set("3180ca260b82a7") // prevents querying for the staging profile id, performance optimization
+		}
+	}
+}
+
+subprojects {
+	if (listOf("client, proto").contains(this.name)) {
+		publishing {
+			publications {
+				create<MavenPublication>("maven") {
+					groupId = "io.provenance.objectstore.gateway"
+					artifactId = this.name
+
+					from(components["java"])
+
+					pom {
+						name.set("Provenance Object Store Gateway Client Library")
+						description.set("A library for interacting with the Provenance Object Store Gateway")
+						url.set("https://provenance.io")
+
+						licenses {
+							license {
+								name.set("The Apache License, Version 2.0")
+								url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+							}
+						}
+
+						developers {
+							developer {
+								id.set("piercetrey-figure")
+								name.set("Pierce Trey")
+								email.set("ptrey@figure.com")
+							}
+						}
+
+						scm {
+							connection.set("git@github.com:provenance-io/object-store-gateway.git")
+							developerConnection.set("git@github.com:provenance-io/object-store-gateway.git")
+							url.set("https://github.com/provenance-io/object-store-gateway")
+						}
+					}
+				}
+			}
+
+			signing {
+				sign(publishing.publications["maven"])
+			}
+
+			tasks.javadoc {
+				if(JavaVersion.current().isJava9Compatible) {
+					(options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+				}
+			}
 		}
 	}
 }
--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.statistics.ReportStatisticsToElasticSearch.url
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
@@ -9,8 +8,6 @@ buildscript {
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    Plugins.MavenPublish.addTo(this)
-    Plugins.Signing.addTo(this)
 }
 
 repositories {
@@ -37,52 +34,4 @@ dependencies {
         *Dependencies.BouncyCastle.all(),
         *Dependencies.Provenance.all(),
     ).forEach { it.implementation(this) }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "io.provenance.objectstore.gateway"
-            artifactId = "client"
-
-            from(components["java"])
-
-            pom {
-                name.set("Provenance Object Store Gateway Client Library")
-                description.set("A library for interacting with the Provenance Object Store Gateway")
-                url.set("https://provenance.io")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("piercetrey-figure")
-                        name.set("Pierce Trey")
-                        email.set("ptrey@figure.com")
-                    }
-                }
-
-                scm {
-                    connection.set("git@github.com:provenance-io/object-store-gateway.git")
-                    developerConnection.set("git@github.com:provenance-io/object-store-gateway.git")
-                    url.set("https://github.com/provenance-io/object-store-gateway")
-                }
-            }
-        }
-    }
-
-    signing {
-        sign(publishing.publications["maven"])
-    }
-
-    tasks.javadoc {
-        if(JavaVersion.current().isJava9Compatible) {
-            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-        }
-    }
 }
