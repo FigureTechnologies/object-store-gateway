286b90521d207f7fc01fa51e19a4f210684972e7
hyperschwartz <jschwartz@figure.com>
2022-09-19 14:31:15 -0700

    Populate blank comment

--- server/src/main/kotlin/io/provenance/objectstore/gateway/eventstream/GatewayEvent.kt
+++ server/src/main/kotlin/io/provenance/objectstore/gateway/eventstream/GatewayEvent.kt
@@ -31,7 +31,12 @@ class GatewayEvent(sourceEvent: TxEvent) : GatewayEventAdapter(sourceEvent) {
         private val logger = KotlinLogging.logger {}
 
         /**
+         * Attempts to instantiate a GatewayEvent with the given TxEvent.  Each value derivation will throw an exception
+         * when the value is not found, which will result in the catch being used to return a null value.
          *
+         * @param sourceEvent The event-stream TxEvent to use as the instantiation source for the GatewayEvent.  Values
+         * are eagerly pulled from the contained attributes, which will either result in a fully-formed GatewayEvent,
+         * or an exception.
          */
         fun fromEventOrNull(sourceEvent: TxEvent): GatewayEvent? = try {
             GatewayEvent(sourceEvent)
