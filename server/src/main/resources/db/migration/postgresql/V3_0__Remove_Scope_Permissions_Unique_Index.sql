d28adb9ac71c8ce87b8a475f388cd58c75b45fab
hyperschwartz <jschwartz@figure.com>
2022-09-28 09:20:32 -0700

    Update V3_0 migration for postgres to be even more safe

--- server/src/main/resources/db/migration/postgresql/V3_0__Remove_Scope_Permissions_Unique_Index.sql
+++ server/src/main/resources/db/migration/postgresql/V3_0__Remove_Scope_Permissions_Unique_Index.sql
@@ -1,3 +1,6 @@
+-- The original index from V1_1 may have been established as a constraint.  This will ensure the constraint is removed
+-- before attempting to drop the index.
+ALTER TABLE scope_permissions DROP CONSTRAINT IF EXISTS scope_permissions_scope_address_grantee_address_granter_address;
 -- Remove old index that enforces unique on scope_address/grantee_address/granter_address in preparation for new index
 -- in the v3_1 migration.
-ALTER TABLE scope_permissions DROP CONSTRAINT IF EXISTS scope_permissions_scope_address_grantee_address_granter_address;
+DROP INDEX IF EXISTS scope_permissions_scope_address_grantee_address_granter_address;
